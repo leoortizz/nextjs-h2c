b15737c257ac9fcdc3ddb002714d8afe156ddaf3
Leonardo Ortiz <leo@monogram.io>
2024-03-14 11:50:49 -0300

    cleanup server.js

--- server.js
+++ server.js
@@ -2,15 +2,11 @@ const next = require("next");
 const http2 = require("node:http2");
 const { parse } = require("node:url");
 
-
 const port = parseInt(process.env.PORT, 10) || 3005;
 const dev = process.env.NODE_ENV !== "production";
 
-// Init the Next app:
 const app = next({ dev });
 
-// Create the secure HTTPS server:
-// Don't forget to create the keys for your development
 const server = http2.createServer();
 
 const handler = app.getRequestHandler();
