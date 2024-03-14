3be2fdb030eda672da6a20e0c3f7d8f131ba966c
Leonardo Ortiz <leo@monogram.io>
2024-03-14 12:41:48 -0300

    server cleanup again :)

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
+// @ts-check
+
 const next = require("next");
 const http2 = require("node:http2");
 const { parse } = require("node:url");
 
-const port = parseInt(process.env.PORT, 10) || 3005;
+const port = parseInt(process.env.PORT || "3005", 10);
 const dev = process.env.NODE_ENV !== "production";
 
-const app = next({ dev });
+const app = next({ dev, port });
 const server = http2.createServer();
 const handler = app.getRequestHandler();
 
 app.prepare().then(() => {
-  server.listen(port);
-  server.on("error", (err) => console.error(err));
+  server.on("error", console.error);
+  server.listen({ port }, () => {
+    console.log(`Listening HTTP2 on port http://localhost:${port}`);
+  });
   server.on("request", (req, res) => {
     const parsedUrl = parse(req.url, true);
     handler(req, res, parsedUrl);
   });
-
-  console.log(`Listening HTTP2 on port http://localhost:${port}`);
 });
